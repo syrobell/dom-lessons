9aa413f91ca4d9abe3d603846a4dcb1897660f63
syrobell <adnandedeogluu@gmail.com>
2023-01-08 00:20:50 +0300

    update

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,3 +1,31 @@
 'use strict';
 
-const
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+btnRoll.addEventListener("click", function() {
+    //Random zar üretme
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    //Gelen sayı Display
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+
+    //1 Gelmesi Durumunu Kontrol Etme
+    if (dice !== 1) {
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    }
+})
