7f7f85049aea7f0fa78befdc966ff3a681a27789
syrobell <adnandedeogluu@gmail.com>
2023-01-08 23:15:04 +0300

    Switch Players Update

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const current0El = document.querySelector("#current--0");
@@ -9,11 +11,14 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener("click", function() {
     //Random zar üretme
@@ -26,6 +31,12 @@ btnRoll.addEventListener("click", function() {
     //1 Gelmesi Durumunu Kontrol Etme
     if (dice !== 1) {
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    } else {
+        document.querySelector(`#current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
     }
 })
