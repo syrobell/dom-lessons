c9750fb050aeb35a68ea88514c367608c326b2ca
Adnan Dedeoğlu <ado_adnen@hotmail.com>
2023-01-17 13:41:40 +0300

    Add Update
    Now project.js can take text from inputs to a object and send it to the ui from ui.js function.

--- MovieProject/project.js
+++ MovieProject/project.js
@@ -12,6 +12,23 @@ function eventListeners() {
 }
 
 function addFilm(e) {
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if (title === "" || director === "" || url === "") {
+        //Eğer ki içeriklerin herhangi biri boşsa
+        //hata mesajı dönecek
+
+
+    } else {
+        // film constructor'ından newFilm adında bir object
+        //oluşturdum
+        const newFilm = new film(title, director, url);
+
+        //Objecti UI'a eklemek
+        ui.addFilmToUI(newFilm);
+    }
 
 
 
--- MovieProject/ui.js
+++ MovieProject/ui.js
@@ -1,3 +1,32 @@
 function UI() {
 
 }
+UI.prototype.addFilmToUI = function(newFilm) {
+    /* <!-- <tr>
+                                             <td><img src="" class="img-fluid img-thumbnail"></td>
+                                             <td></td>
+                                             <td></td>
+                                             <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+                                           </tr> -->
+                                           <!-- <tr>
+                                             <td><img src="" class="img-fluid img-thumbnail"></td>
+                                             <td></td>
+                                             <td></td>
+                                             <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+                                           </tr> -->
+                                           */
+    const filmList = document.getElementById("films");
+
+    filmList.innerHTML += `
+    <tr>
+        <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+        <td>${newFilm.title}</td>
+        <td>${newFilm.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+    </tr>
+    s
+    
+    
+    
+    `;
+}
