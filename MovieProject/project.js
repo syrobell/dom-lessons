eb69ccb772b01ede7650251f5796543c9d0ce7a7
Adnan Dedeoğlu <ado_adnen@hotmail.com>
2023-01-19 17:45:24 +0300

    Update

--- MovieProject/project.js
+++ MovieProject/project.js
@@ -3,7 +3,11 @@ const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector("#director");
 const urlElement = document.querySelector("#url");
 
+// UI prototype obje üretme
 const ui = new UI();
+
+// Storage prototype obje üretme
+const storage = new Storage();
 
 eventListeners();
 
@@ -19,18 +23,20 @@ function addFilm(e) {
     if (title === "" || director === "" || url === "") {
         //Eğer ki içeriklerin herhangi biri boşsa
         //hata mesajı dönecek
+        ui.displayMessages("Lütfen Tüm Alanları Doldurunuz", "danger");
 
 
     } else {
         // film constructor'ından newFilm adında bir object
         //oluşturdum
         const newFilm = new film(title, director, url);
-
+        storage.addFilmToStorage(newFilm);
         //Objecti UI'a eklemek
         ui.addFilmToUI(newFilm);
+        ui.displayMessages("Film Başarıyla Eklendi", "success");
     }
 
-
+    ui.clearInputs(titleElement, directorElement, urlElement);
 
     e.preventDefault();
 }
---
+++ MovieProject/storage.js
@@ -0,0 +1,20 @@
+function Storage {
+
+}
+
+Storage.prototype.addFilmToStorage = function(newFilm) {
+    let films = this.getFilmsFromStorage();
+
+    films.push(newFilm);
+    localStorage.setItem("films", JSON.stringify(films));
+}
+
+Storage.prototype.getFilmsFromStorage = function() {
+    let films;
+
+    if (localStorage.getItem("films") === null) {
+        films = [];
+    } else {
+        films = JSON.parse(localStorage.getItem("films"));
+    }
+}
